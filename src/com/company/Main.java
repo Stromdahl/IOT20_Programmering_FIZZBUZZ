a03fcbf7f8ae4dc3287993ac40203a37a5c6c230
Mattias Strömdahl <Mattias.Strmdahl@gmail.com>
2020-09-07 14:40:47 +0200

    lagt labben i git

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 101; i++) {
+
+            if (i  == 42){
+
+                System.out.println("Answer to the ultimate question of life, the universe and everyting");
+
+            }
+            else if (i % 3 == 0 && i % 5 == 0) {
+
+                System.out.println("FizzBuzz");
+
+            }
+            else if (i % 5 == 0) {
+
+                System.out.println("Buzz");
+
+            }
+            else if (i % 3 == 0 ) {
+
+                System.out.println("Fizz");
+
+            }
+            else {
+
+                System.out.println( i );
+
+            }
+        }
+    }
+}
+
+

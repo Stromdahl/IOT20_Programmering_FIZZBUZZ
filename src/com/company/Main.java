57f41edcb03ef61a030f405331b97c0c983926b3
Besim <Besim.musliu@student.kyh.se>
2020-09-10 16:01:24 +0200

    Made the code alot sexier

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,28 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-
         for (int i = 1; i < 101; i++) {
-
-            if (i  == 42){
-
+            if (i  == 42)
+            {
                 System.out.println("Answer to the Ultimate Question of life, the Universe and Everything");
-
             }
-            else if (i % 3 == 0 && i % 5 == 0) {
-
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
                 System.out.println("FizzBuzz");
-
             }
-            else if (i % 5 == 0) {
-
+            else if (i % 5 == 0)
+            {
                 System.out.println("Buzz");
-
             }
-            else if (i % 3 == 0 ) {
-
+            else if (i % 3 == 0 )
+            {
                 System.out.println("Fizz");
-
             }
-            else {
-
+            else
+                {
                 System.out.println( i );
-
-            }
+                }
         }
     }
 }
